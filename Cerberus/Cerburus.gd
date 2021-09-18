b47d08f79ef393f0bd84f298a52281f4a2c9b11a
OneTachi <n2verz@gmail.com>
2021-09-18 19:41:44 -0400

    create cerburus script

---
+++ Cerberus/Cerburus.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var type_of_attack
+var total_chance
+
+#creates type of attack + chances
+func do_turn():
+	#Creates Chances
+	total_chance = 100
+	var first_head_chance = randi() % 100
+	total_chance -= first_head_chance
+	var second_head_chance = randi() % total_chance
+	total_chance -= second_head_chance
+	var third_head_chance = total_chance
+	
+	#Assign value to type_of_attack
+	type_of_attack = assign_attack(first_head_chance, second_head_chance, third_head_chance)
+	return type_of_attack
+
+func assign_attack(first, second, third):
+	var roll_of_die = randi() % 100 + 1
+	
+	if roll_of_die <= first:
+		return "first"
+	elif roll_of_die <= first + second:
+		return "second"
+	else:
+		return "third"
