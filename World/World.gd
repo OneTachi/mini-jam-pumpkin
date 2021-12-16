ea7272a281f386a5493eedc1d38acb40c7d2101b
OneTachi <n2verz@gmail.com>
2021-12-16 17:59:34 -0500

    redo attack manager

--- World/World.gd
+++ World/World.gd
@@ -20,26 +20,29 @@ var arrow_rotations = {
 	'ui_up': -90,
 	'ui_down': 90, 
 }
-var combat_turn = 0
 var neededInputKey
-var in_combat = false
-var won_combat = 0 
+var inCombat = false
+var combatTurn = -1
+var wonAttacks = 0
+var doesPlayerAttack
 
-enum {
-	guard,
-	attack
-}
+var tFights = ['defend', 'attack']
+var nFights
+var oFights = []
 
-var playerInput
 
 func _ready():
+	arrow_sprite.visible = false
 	do_cerb_turn('None')
 
 func turn_manager(type, option):
-	playerInput = type
-	#print(str(option) + ' ' + str(cerb_att))
-	match playerInput:
-		'guard':
+	if !inCombat:
+		create_fights()
+	#var matchInput = oFights.pop_back() FOR NORMAL PURPOSE
+	var matchInput = 'attack' #For Testing
+	
+	match matchInput:
+		'defend':
 			if option == cerb_att:
 				take_heal(player, 50, 25, 1)
 				take_damage(cerb, 20, player.stats.base_dmg, 2)
@@ -50,21 +53,68 @@ func turn_manager(type, option):
 			do_cerb_turn(text_status)
 		
 		'attack':
-			neededInputKey = null
-			if combat_turn > 4:
-				in_combat = false
-				won_combat = 0
-				combat_turn = 0
-			else:
-				do_combat()
-				in_combat = true
-			update_text()
-				#TODO After battle stuff
-			
-			#take_damage(cerb, 20, player.stats.base_dmg,1)
-			#take_damage(player, 30, cerb.stats.base_dmg, 1)
-			#text_status = "Attacked and hit!"
+			attack_manager(0)
 	check_win()
+
+func attack_manager(status, combat = inCombat):
+	print(wonAttacks)
+	if !combat:
+		inCombat = true
+	combatTurn += 1
+	neededInputKey = null
+	arrow_sprite.visible = false
+	timer.stop()
+	
+	if status > 0: 
+		wonAttacks += 1
+	
+	if combatTurn > 4:
+		inCombat = false
+		wonAttacks = 0
+		combatTurn = -1 
+	else: 
+		timer.start(randi() % 2 + .1)
+
+func check_win():
+	if cerb.stats.health <= 0:
+		print('You Win!')
+	elif player.stats.health <= 0:
+		print('You lose...')
+
+#TODO
+func do_attack():
+	var keys = dir_keys.duplicate()
+	keys.shuffle()
+	var direction = keys.pop_front()
+	neededInputKey = direction
+	pop_key(direction)
+	#debug_number += 1
+	#print(debug_number)
+
+func pop_key(dir):
+	arrow_sprite.visible = true
+	arrow_sprite.rotation_degrees = arrow_rotations[dir] 
+	timer.start(rand_range(.2, 1))
+
+func create_fights():
+	oFights.clear()
+	nFights = randi() % 4 + 1
+	var fightArray = tFights.duplicate()
+	fightArray.shuffle()
+	for x in nFights:
+		oFights.push_back(fightArray.pop_front())
+
+func _unhandled_input(event):
+	if event is InputEventKey && neededInputKey != null:
+		if event.is_action_pressed(neededInputKey):
+			if inCombat:
+				attack_manager(1)
+
+func _on_Timer_timeout():
+	if neededInputKey != null:
+		attack_manager(0)
+	else:
+		do_attack()
 
 func do_cerb_turn(status):
 	cerb_return = cerb.do_turn()
@@ -82,54 +132,8 @@ func update_text():
 	 '\nCerburus Health: ' + str(cerb.stats.health) + '\nHealth: ' 
 	+ str(player.stats.health) + '\nStatus: ')
 
-func check_win():
-	if cerb.stats.health <= 0:
-		print('You Win!')
-	elif player.stats.health <= 0:
-		print('You lose...')
-
-#TODO
-func do_combat():
-	var keys = dir_keys.duplicate()
-	keys.shuffle()
-	var direction = keys.pop_front()
-	neededInputKey = direction
-	pop_key(direction)
-	#debug_number += 1
-	#print(debug_number)
-
-func pop_key(dir):
-	arrow_sprite.rotation_degrees = arrow_rotations[dir] 
-	timer.start(.5)
-
-func handle_input(key):
-	if !in_combat:
-		return
-	handle_one_combat_result('won')
-
-func handle_one_combat_result(result):
-	if result != null:
-		won_combat += 1
-		take_damage(cerb, 20, 20, 1)
-		take_heal(player, 10, 5, 1)
-	else:
-		take_damage(player, 20, 10, 1)
-		take_heal(cerb, 10, 1, 1)
-	combat_turn += 1
-	timer.stop()
-	turn_manager('attack', null)
-
 func take_damage(who, iRange, base, multiplier):
 	who.stats.health -= (randi() % iRange + base) * multiplier
 
 func take_heal(who, iRange, base, multiplier):
 	who.stats.health += (randi() % iRange + base) * multiplier
-
-func _unhandled_input(event):
-	if event is InputEventKey && neededInputKey != null:
-		if event.is_action_pressed(neededInputKey):
-			handle_input(event)
-
-
-func _on_Timer_timeout():
-	handle_one_combat_result(null)
